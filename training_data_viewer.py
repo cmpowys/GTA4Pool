2136dc3270bcdb530b8b8849ce7f839f13737f23
= <callum.powys@gmail.com>
2023-01-08 00:48:29 +0000

    Added coloured bounding boxes to match with the colour of the pool ball. Need to work on getting better bounding boxes.

--- training_data_viewer.py
+++ training_data_viewer.py
@@ -3,6 +3,18 @@ import pickle
 import os
 import config
 
+colourings = {
+    'red' : (255, 0, 0),
+    'purple' : (90,0,90), ##800080
+    'black' : (0, 0, 0),
+    'white' : (255, 255, 255),
+    'green' : (0, 255, 0),
+    'blue' : (0, 0, 255),
+    'brown' : (165,42,42),#A52A2A
+    'yellow' : (255, 255, 0),
+    'orange' : (255, 165, 0) ##FFA500
+}
+
 def main():
     
     metadata = pickle.load(open(config.TRAINING_METADATA_FILENAME, "rb"))
@@ -16,7 +28,8 @@ def main():
 
             for label in metadata_entry:
                 bounding_box = metadata_entry[label]
-                cv2.rectangle(img, bounding_box[0], bounding_box[1], (50, 50, 50), 3) 
+                colour = get_colour_from_label(label)
+                cv2.rectangle(img, bounding_box[0], bounding_box[1], colour, 3) 
 
             cv2.imshow(config.DISPLAY_WINDOW_NAME, img)
             cv2.waitKey(None)
@@ -24,6 +37,12 @@ def main():
     finally:
         cv2.destroyAllWindows()
 
+def get_colour_from_label(label):
+    for colour in colourings:
+        if colour in label:
+            c = colourings[colour]
+            return c[2], c[1], c[0]
+    assert(False)
  
 if __name__ == "__main__":
     main()
